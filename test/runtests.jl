5f2c2686c9ff63b662dac8808100e68ad8e26a08
Jae-Mo Lihm <jaemolim96@gmail.com>
2022-12-26 09:55:47 +0900

    Add test of analytic continuation

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,4 +18,5 @@ using Test
     include("test_model_siam.jl")
 
     include("test_parquet.jl")
+    include("test_analytic_continuation.jl")
 end
---
+++ test/test_analytic_continuation.jl
@@ -0,0 +1,81 @@
+using mfRG
+using Test
+
+@testset "analytic continuation" begin
+    # Analytic continuation from KF to MF
+    e = 0.5
+    Δ = 1.0
+    t = 0.5
+    G0 = SIAMLazyGreen2P{:MF}(; e, Δ, t)
+    basis_MF = ImagGridAndTailBasis(:Fermion, 1, 3, 20)
+    G_MF = mfRG.green_lazy_to_explicit(G0, basis_MF)
+
+    G0 = SIAMLazyGreen2P{:KF}(; e, Δ, t)
+    basis_KF = LinearSplineAndTailBasis(1, 3, get_nonequidistant_grid(6, 151))
+    G_KF = mfRG.green_lazy_to_explicit(G0, basis_KF)
+
+    vs = -12:11
+    G_MF_ac = analytic_continuation_KF_to_MF(G_KF, basis_MF, t, :Green)
+    @test G_MF_ac.(vs) ≈ G_MF.(vs) rtol=1e-4
+end
+
+@testset "analytic continuation SIAM parquet" begin
+    # Test analytic continuation from KF to MF of the SIAM parquet result
+    e = 0.5
+    Δ = 1.0
+    t = 0.5
+    U = 2.0
+    function do_parquet(F)
+        if F === :MF
+            nmax = 4
+            basis_1p = ImagGridAndTailBasis(:Fermion, 1, 3, nmax * 3 + 10)
+            basis_w_k1 = ImagGridAndTailBasis(:Boson, 1, 0, 4 * nmax)
+            basis_w = ImagGridAndTailBasis(:Boson, 1, 0, 2 * nmax)
+            basis_v_aux = ImagGridAndTailBasis(:Fermion, 1, 0, nmax)
+            basis_w_bubble = ImagGridAndTailBasis(:Boson, 1, 0, maximum(get_fitting_points(basis_w_k1)))
+            basis_v_bubble = ImagGridAndTailBasis(:Fermion, 2, 4, maximum(get_fitting_points(basis_w_k1)))
+        elseif F === :KF
+            vgrid_1p = get_nonequidistant_grid(8, 51) .* Δ
+            vgrid_k1 = get_nonequidistant_grid(8, 11) .* Δ
+            wgrid_k1 = get_nonequidistant_grid(8, 11) .* Δ
+            vgrid_k3 = get_nonequidistant_grid(8, 11) .* Δ
+
+            basis_1p = LinearSplineAndTailBasis(1, 3, vgrid_1p)
+            basis_w = LinearSplineAndTailBasis(1, 3, wgrid_k1)
+            basis_w_k1 = LinearSplineAndTailBasis(1, 3, wgrid_k1)
+            basis_v_aux = LinearSplineAndTailBasis(1, 0, vgrid_k3)
+            basis_v_bubble_tmp = LinearSplineAndTailBasis(2, 4, vgrid_k1)
+            basis_v_bubble, basis_w_bubble = basis_for_bubble(basis_v_bubble_tmp, basis_w)
+        end
+        G0 = SIAMLazyGreen2P{F}(; e, Δ, t)
+        Γ, Σ, Π = run_parquet(G0, U, basis_v_bubble, basis_w_bubble, basis_w_k1, basis_w,
+            basis_v_aux, basis_1p; max_iter=30, reltol=1e-3, temperature=t, mixing_coeff=1.0);
+        G = solve_Dyson(G0, Σ)
+        op_suscep_L, op_suscep_R = susceptibility_operator_SU2(Val(F))
+        chi = compute_response_SU2(op_suscep_L, op_suscep_R, Γ, Π.A)
+        n = compute_occupation(G, t)
+        (; Γ, Σ, Π, G, chi, n, basis_1p, basis_w)
+    end
+
+    res_MF = do_parquet(:MF)
+    res_KF = do_parquet(:KF)
+
+    @test res_MF.n ≈ res_KF.n rtol=5e-3
+
+    vs = -12:11
+    G_MF_ac = analytic_continuation_KF_to_MF(res_KF.G, res_MF.basis_1p, t, :Green)
+    Σ_MF_ac = analytic_continuation_KF_to_MF(res_KF.Σ, res_MF.basis_1p, t, :Vertex)
+    @test G_MF_ac.(vs) ≈ res_MF.G.(vs) rtol=5e-3
+    @test Σ_MF_ac.(vs) ≈ res_MF.Σ.(vs) rtol=5e-3
+    @test Σ_MF_ac(10000) ≈ res_MF.Σ(10000) rtol=5e-2  # Hartree term has larger error
+
+    ws = -10:10
+    basis_w_ac = ImagGridAndTailBasis(:Boson, 1, 3, 8)
+    for key in (:total, :disconnected, :connected), i in 1:2
+        chi_MF = getproperty(res_MF.chi, key)[i]
+        chi_KF = getproperty(res_KF.chi, key)[i]
+        chi_MF_ac = analytic_continuation_KF_to_MF(chi_KF, basis_w_ac, t, :Green)
+        @test chi_MF_ac.(ws) ≈ chi_MF.(ws) rtol=2e-1  # susceptibility has larger error
+        @test maximum(abs.(getindex.(chi_MF_ac.(ws) .- chi_MF.(ws), 1, 1))) < 0.05
+    end
+end
